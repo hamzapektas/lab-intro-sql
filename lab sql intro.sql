8e3a1db95b46c364f3fd7bd5bd304f10c2c9ab3f
Hamza <hamzapektas@gmail.com>
2021-08-19 09:01:05 +0200

    Lab SQL intro Hamza

--- lab sql intro.sql
+++ lab sql intro.sql
@@ -1,18 +1,38 @@
 USE sakila;
+
+-- 1 Review the tables in the database.
+
 SHOW tables;
-DESCRIBE film;
+
+-- 2 Explore tables by selecting all columns from each table or using the in built review features for your client.
+
+SELECT * FROM actor;
 SELECT * FROM film;
+SELECT * FROM customer;
+-- ...
+
+-- 3 Select one column from a table. Get film titles.
+
 SELECT title FROM sakila.film;
-show tables;
-SELECT * FROM columns;
-SELECT name as 'language'
-FROM language;
-SELECT actor_id FROM actor;
-SELECT * FROM store;
--- number 5.1
-SELECT COUNT(DISTINCT store_id) AS NumberOfStores FROM store;
-SELECT  staff_id
-FROM staff; 
+
+/* 4 Select one column from a table and alias it. Get unique list of film languages 
+under the alias language. Note that we are not asking you to obtain the language 
+per each film, but this is a good time to think about how you might get that information 
+in the future.*/
+
+SELECT distinct name AS 'language' 
+FROM sakila.language;
+
+-- 5.1 Find out how many stores does the company have?
+
+SELECT count(store_id) FROM store;
+
+-- 5.2 Find out how many employees staff does the company have?
+
+SELECT count(staff_id) FROM staff;
+
+-- 5.3 Return a list of employee first names only?
+
 SELECT first_name
 FROM staff;
 
